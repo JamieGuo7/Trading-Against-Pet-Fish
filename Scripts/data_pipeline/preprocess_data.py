7ff66dae7d5cc60bd40da180df88a24e61ad95e5
Jamie Guo <89688003+JamieGuo7@users.noreply.github.com>
2025-12-14 15:14:49 +0000

    Removed erroneous tickers and downloaded data for all ESGU stocks.

--- Scripts/data_pipeline/preprocess_data.py
+++ Scripts/data_pipeline/preprocess_data.py
@@ -4,26 +4,31 @@ import yfinance as yf
 from datetime import datetime
 
 # Getting data
-tech = ['GOOG', 'MSFT', 'AAPL', 'NVDA', 'AMZN']
-data = yf.download(tech, period = '1y', interval='1wk')
+tickers = []
 
-# Cleaning and Structuring
-data.dropna(inplace = True)
+with open('data/ESGU_Tickers.txt', 'r') as file:
+    for line in file:
+        tickers.append(line.strip())
+
+
+# Downloading and tidying data
+raw_data = yf.download(tickers,
+                           period = '1y',
+                           interval='1wk',
+                           progress = True,
+                           auto_adjust = True) # Fix Warning
+raw_data.dropna(inplace = True)
+
 tidy_data = (
-    data
-    .stack()     # stack tickers
-    .stack()     # stack fields
-    .reset_index()
-)
+        raw_data
+        .stack()     # stack tickers
+        .stack()     # stack fields
+        .reset_index()
+    )
 
 tidy_data.columns = ["Date", "Ticker", "Type", "Price"]
-
-# Sort nicely
 tidy_data = tidy_data.sort_values(["Date", "Ticker", "Type"]).reset_index(drop=True)
+tidy_data = tidy_data[tidy_data['Type'] == 'Close']
 
-
-print(tidy_data.info())
-print(tidy_data.head(10))
-
-file_path = './data/tech_data.csv'
+file_path = './data/ESGU_data.csv'
 tidy_data.to_csv(file_path, index = False)
